4139ea97ae4973347cf5dbcf2a80b0013a320925
Zephelion <russellnumo@outlook.com>
2023-02-14 21:53:19 +0100

    loop over array and append to html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const apiKey = "9ht5U2BA";
+const container = document.querySelector("main ul");
+
+console.log(container);
 
 const fetchArt = async () => {
   const response = await fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${apiKey}&involvedMaker=Rembrandt+van+Rijn`);
   const data = await response.json();
-  console.log(data);
+  
+  const paintings = data.artObjects;
+  displayArt(paintings);
 }
 
-// fetchArt();
+const displayArt = (paintings) => {
+    container.innerHTML = "";
+
+    paintings.forEach(painting => {
+        var liHtml = `<li><img src="${painting.webImage.url}" alt="" srcset=""></li>`;
+
+        container.innerHTML += liHtml;
+    });
+}
+
+fetchArt();
